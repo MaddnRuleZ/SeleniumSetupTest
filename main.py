927397fb89971277ba599f9e58dc3876478ff847
MaddnRuleZ <106453966+MaddnRuleZ@users.noreply.github.com>
2024-05-23 15:16:38 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--remote-debugging-port=9222')
+
+driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=options)
+driver.get('http://www.google.com')
+print(driver.title)
+driver.quit()
