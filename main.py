30a9cb7e92cca9038b2408e3c3899148cdfcac8c
MaddnRuleZ <106453966+MaddnRuleZ@users.noreply.github.com>
2024-05-23 15:18:27 +0200

    sw

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--remote-debugging-port=9222')
+options.binary_location = "/usr/local/bin/chromedriver"
 
-driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=options)
+driver = webdriver.Chrome(options=options)
 driver.get('http://www.google.com')
 print(driver.title)
 driver.quit()
